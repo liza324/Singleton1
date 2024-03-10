9caad746ddd86099fb6c3f76c0fa60fbd21bfaa3
Lizz <golubl692@gmail.com>
2024-03-10 19:55:44 +0100

    Add project files.

---
+++ wzorzec1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace LogowanieKomunikatow
+{
+    public class Log
+    {
+        private static Log _instance;
+        private List<string> logMessages;
+
+        private Log()
+        {
+            logMessages = new List<string>();
+        }
+
+        public static Log GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Log();
+            }
+            return _instance;
+        }
+
+        public void LogMessage(string message)
+        {
+            logMessages.Add(message);
+
+        }
+
+        public List<string> GetLogMessages()
+        {
+            return logMessages;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log log1 = Log.GetInstance();
+            log1.LogMessage("komunikat logowania 1");
+            Log log2 = Log.GetInstance();
+            log2.LogMessage("komunikat logowania 2");
+
+            Console.WriteLine("Komunikaty logowania z log1");
+            foreach (var message in log1.GetLogMessages())
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.WriteLine("Komunikaty logowania z log2");
+            foreach (var message in log2.GetLogMessages())
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
+
